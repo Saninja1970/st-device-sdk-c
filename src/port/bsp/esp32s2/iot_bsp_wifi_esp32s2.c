786c26ce9e09139bd0591a9884a3adba3025b4e5
Ananth <ananthudupa2002@gmail.com>
2023-06-08 17:11:49 +0530

    Code changes

--- src/port/bsp/esp32s2/iot_bsp_wifi_esp32s2.c
+++ src/port/bsp/esp32s2/iot_bsp_wifi_esp32s2.c
@@ -37,9 +37,9 @@
 #define EXAMPLE_ESP_WIFI_PASS      ""
 #define EXAMPLE_ESP_MAXIMUM_RETRY  10
 
-#define DUMMY_TARGET_WIFI_SSID    "AnnapornaSF"
+#define DUMMY_TARGET_WIFI_SSID    "AmoghPhone"
 
-uint8_t DUMMY_TARGET_WIFI_BSSID[] = {0X4A , 0X1E , 0XBB , 0XEC , 0XA8 , 0XC7 };
+uint8_t DUMMY_TARGET_WIFI_BSSID[] = {0X26 , 0XAE , 0X22 , 0XD5 , 0XE3 , 0XEC };
 
 const int WIFI_STA_START_BIT 		= BIT0;
 const int WIFI_STA_CONNECT_BIT		= BIT1;
@@ -634,8 +634,8 @@ uint16_t iot_bsp_wifi_get_scan_result(iot_wifi_scan_result_t *scan_result)
 			free(ap_list);
 			memcpy(scan_result[ap_num].ssid, DUMMY_TARGET_WIFI_SSID, strlen(DUMMY_TARGET_WIFI_SSID));
 			memcpy(scan_result[ap_num].bssid, DUMMY_TARGET_WIFI_BSSID , IOT_WIFI_MAX_BSSID_LEN);
-			scan_result[ap_num].rssi = -43 ;
-			scan_result[ap_num].freq = 2457 ;
+			scan_result[ap_num].rssi = -21 ;
+			scan_result[ap_num].freq = 2437 ;
 			scan_result[ap_num].authmode = IOT_WIFI_AUTH_WPA_WPA2_PSK ;
 			ap_num = ap_num +1;
 		} 
